1bf37b086ff318567b680e0ba28cf9b753416b5c
Robert Martin <rmartin@wipeos.com>
2025-05-13 11:43:02 -0500

    [check_certs]  look at ALL certs, make more verbose messaging

--- check_certs/check_certs.py
+++ check_certs/check_certs.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 from utility import *
+from datetime import datetime
 import dateutil.parser #  sudo apt install python3-dateutil
 
+EXPIRY = 30
 today = datetime.now().date()
-one_month_from_now = today + timedelta(days=30)
+one_month_from_now = today + timedelta(days=EXPIRY)
 
 def open_cert(filepath):
   return Command('openssl x509 -noout -text -in {}'.format(filepath)).run(1)
@@ -13,41 +15,35 @@ def expiration(cert):
   line = line.split(':', 1)
   return dateutil.parser.parse(line[1].strip()).date()
 
-
-#c = open_cert('/opt/certificate_authority/intermediate/clients/cert/2024.cert.p
-em')
+#c = open_cert('/opt/certificate_authority/intermediate/clients/cert/2024.cert.pem')
 #e = expiration(c)
 
 immediately_failing = []
 soon_to_fail = []
-
-
-'''
-Yeah, this sucks! but we need to have this for sanity's sake
-it works, its written this way because i needed to get something done quick
-'''
 
 def check_all_certs(filepath='/opt/certificate_authority/intermediate/clients/cert/'):
   certs = Command('find  {} -type f'.format(filepath)).run(1).split('\n')
   for cert in certs:
     c = open_cert(cert)
     exp = expiration(c)
-    cert_id = re.findall('CN = [0-9]{4}', c)
-    if cert_id:
-      cert_id = cert_id[0].split(' = ')[-1]
-    else:
-      cert_id = 'unknown'
+    cert_id = re.search('Subject.*CN = ([A-Z0-9a-z]*)', c)
     if exp < today:
-      p('{}, {}'.format(cert_id, exp))
-      immediately_failing.append('{}, {}'.format(cert_id, exp))
+      immediately_failing.append((exp, cert_id[1]))
     elif exp < one_month_from_now:
-      p('{}, {}'.format(cert_id, exp))
-      soon_to_fail.append('{}, {}'.format(cert_id, exp))
+      soon_to_fail.append((exp, cert_id[1]))
 
 check_all_certs()
 
-output = 'These certs are going to fail in less than 30 days:\n'
-output += '\n'.join(soon_to_fail)
+output = ''
+#output = 'These certs have already failed. FIX NOW!!!!\n'
+#cert_list = sorted(immediately_failing)
+#for i in sorted(cert_list):
+#  output += 'cert: ' + str(i[1]) + ' -- ' + i[0].strftime('%Y-%m-%d') + '\n'
+output += '\n\nThese certs are going to fail in less than ' + str(EXPIRY) + ' days:\n'
+cert_list = sorted(soon_to_fail)
+for s in sorted(cert_list):
+  output += 'cert: ' + str(s[1]) + ' -- ' + s[0].strftime('%Y-%m-%d') + '\n'
+
 # copied the /etc/ssmtp/ssmtp.conf from the appliances, it uses our existing infrastructure:
 if any(soon_to_fail):
   Command('echo "{}" | mail -s "Certs going to expire" developers@wipeos.com'.format(output)).run(1)
