d049c1a1b9380cf4056fff1074bba3ddcdf67bcf
Robert F.K. Martin <rmartin@wipeos.com>
2025-05-13 15:36:03 -0500

    [check_certs] now pairs cert info with database to get more detailed email

--- check_certs/README.md
+++ check_certs/README.md
@@ -5,3 +5,5 @@ folklore/10.3.3.39
 /home/user/bin
 ## Run
 cron job via user crontab ( 0 4 * * * /home/user/bin/check_certs )
+## Description
+This finds the expiration of all certificates ever created and creates a list of those expiring within EXPIRY days. This list of wipeboxes is then used to gather more information like company name, wipebox name, and last sync and then this list is sent to developers@wipeos.com
--- check_certs/check_certs.py
+++ check_certs/check_certs.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
-from utility import *
-from datetime import datetime
-import dateutil.parser #  sudo apt install python3-dateutil
+import subprocess
+import os
+import re
+import datetime
+import dateutil.parser
+import psycopg2
+import requests
+import json
+import logging
 
+SERVER = 'login.wipeos.com'
+PORT = 17771
+ENDPOINT = 'send_email'
 EXPIRY = 30
-today = datetime.now().date()
-one_month_from_now = today + timedelta(days=EXPIRY)
+today = datetime.datetime.now().date()
+one_month_from_now = today + datetime.timedelta(days=EXPIRY)
+
+logging.basicConfig(filename='check_certs.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+check_certs_logger = logging.getLogger('check_certs')
+
+def list_files_by_type(directory, extension):
+    files_of_type = [
+        os.path.join(directory, f)
+        for f in os.listdir(directory)
+        if os.path.isfile(os.path.join(directory, f)) and f.lower().endswith(extension.lower())
+    ]
+    return files_of_type
 
 def open_cert(filepath):
-  return Command('openssl x509 -noout -text -in {}'.format(filepath)).run(1)
+  return subprocess.check_output(['openssl', 'x509', '-noout', '-text', '-in', filepath]).decode('utf-8')
 
 def expiration(cert):
   line = [x for x in cert.split('\n') if 'Not After :' in x][0]
   line = line.split(':', 1)
   return dateutil.parser.parse(line[1].strip()).date()
 
-#c = open_cert('/opt/certificate_authority/intermediate/clients/cert/2024.cert.pem')
-#e = expiration(c)
-
 immediately_failing = []
 soon_to_fail = []
 
-def check_all_certs(filepath='/opt/certificate_authority/intermediate/clients/cert/'):
-  certs = Command('find  {} -type f'.format(filepath)).run(1).split('\n')
+def check_all_certs(filepath='/home/rfkmartin/certs'):
+  certs = list_files_by_type(filepath, ".pem")
   for cert in certs:
     c = open_cert(cert)
     exp = expiration(c)
@@ -34,16 +51,38 @@ def check_all_certs(filepath='/opt/certificate_authority/intermediate/clients/ce
 
 check_all_certs()
 
-output = ''
-#output = 'These certs have already failed. FIX NOW!!!!\n'
-#cert_list = sorted(immediately_failing)
-#for i in sorted(cert_list):
-#  output += 'cert: ' + str(i[1]) + ' -- ' + i[0].strftime('%Y-%m-%d') + '\n'
-output += '\n\nThese certs are going to fail in less than ' + str(EXPIRY) + ' days:\n'
-cert_list = sorted(soon_to_fail)
-for s in sorted(cert_list):
-  output += 'cert: ' + str(s[1]) + ' -- ' + s[0].strftime('%Y-%m-%d') + '\n'
+body = '<html><h2>These certs are going to fail in less than {} days:</h2>'.format(EXPIRY)
+try:
+  connection = psycopg2.connect(
+      host="login.wipeos.com",
+      database="wipeos_portal",
+      user="wipeos",
+      password="WiPe332"
+  )
+  cursor = connection.cursor()
 
-# copied the /etc/ssmtp/ssmtp.conf from the appliances, it uses our existing infrastructure:
-if any(soon_to_fail):
-  Command('echo "{}" | mail -s "Certs going to expire" developers@wipeos.com'.format(output)).run(1)
+  body += '<br><table border="1"><tr><th align ="left">Company</th><th align ="left">Wipebox</th><th align ="left">Cert</th><th align ="left">Expiration Date</th><th align ="left">Last Sync</th></tr>'
+  cert_list = sorted(soon_to_fail)
+  for s in sorted(cert_list):
+    cursor.execute("SELECT name,system_id,(select value from wipers_settings where setting='siteid' and system_id=ww.system_id),last_sync from wipers_wipebox ww join account_account aa on aa.id=ww.account_id where system_id={}".format(s[1]))
+    last_sync = cursor.fetchall()
+    for l in last_sync:
+      body += '<tr><td align ="left">{}</td><td align ="left">{}</td><td align ="left">{}</td><td align ="left">{}</td><td align ="left">{}</td></tr>'.format(l[0],l[2],s[1],s[0].strftime('%Y-%m-%d'),l[3].strftime('%Y-%m-%d'))
+  body += '</table></html>'
+  data = {"to": "rfkmartin@gmail.com","subject": "Appliance Certificate Expiration Check", "body": body, "mimetype": "html"}
+  headers = {"Content-Type": "application/json"}
+  json_data = json.dumps(data)
+  dest = 'http://{}:{}/{}'.format(SERVER, PORT, ENDPOINT)
+  try:
+        response = requests.post(dest, data=json_data, headers=headers)
+        check_certs_logger.info("Response: {}, {}".format(response.status_code, response.text))
+  except Exception as e:
+      check_certs_logger.error("An unexpected error occurred: {}".format(e))
+      check_certs_logger.info("FAILURE: {}".format(json_data))
+except psycopg2.Error as error:
+    check_certs_logger.error("Error while connecting to PostgreSQL", error) #error checking for actual connection?
+    check_certs_logger.info("FAILURE: {}".format(json_data))
+finally: #end closing connection
+    if connection:
+        cursor.close()
+        connection.close()
