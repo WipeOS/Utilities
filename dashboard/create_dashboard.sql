9ff4a99a79991305311aaaa01ace30c8ebe6c882
Robert F.K. Martin <rmartin@wipeos.com>
2025-06-04 15:50:55 -0500

    [db_dashboard] fix endpts, connect to portal [create_dashboard] fixing schema

--- dashboard/create_dashboard.sql
+++ dashboard/create_dashboard.sql
@@ -24,12 +24,14 @@ create table mount_point (
 );
 
 create table disk_usage (
+   disk_usage_id int not null auto_increment,
    mount_point_id int not null,
    wipebox_id int not null,
    total int not null,
    used int not null,
    available int not null,
-   primary key(mount_point_id, wipebox_id),
+   percent_used int not null,
+   primary key(disk_usage_id),
    foreign key (mount_point_id) references mount_point(mount_point_id),
    foreign key (wipebox_id) references wipebox(wipebox_id)
 );
@@ -47,6 +49,15 @@ create table repo_version (
    hash varchar(16) not null,
    foreign key (repository_id) references repository(repository_id),
    foreign key (version_id) references version(version_id)
+);
+
+create table generic_config (
+   configuration_id int not null auto_increment,
+   wipebox_id int not null,
+   name varchar(128) not null,
+   value json not null,
+   primary key (configuration_id),
+   foreign key (wipebox_id) references wipebox(wipebox_id)
 );
 
 insert into repository (repo_name, remote_url) values ('ApplianceUId', 'github.com:WipeOS/ApplianceUId.git');
@@ -68,3 +79,7 @@ insert into mount_point (mount_point) values ('/var/volatile/log');
 insert into mount_point (mount_point) values ('/var/volatile/tmp');
 insert into mount_point (mount_point) values ('/var/lib/postgresql');
 insert into mount_point (mount_point) values ('/var/lib/postgresql/images');
+
+insert into version (version_string) values ('2.4.18-DEV');
+
+-- select wipebox_id,concat("<table><tr><th>Mount Point</th><th>Size</th><th>Percent Used</th></td>",group_concat(tble order by wipebox_id separator ''),"</table>") from (select wipebox_id,concat("<tr><td>",mount_point,"</td><td>",total,"G</td><td>(",percent_used,"%)</td></tr>") as tble from disk_usage du join mount_point mp on du.mount_point_id=mp.mount_point_id order by wipebox_id) t group by wipebox_id;
--- dashboard/db_dashboard.py
+++ dashboard/db_dashboard.py
@@ -7,6 +7,8 @@ import math
 import shlex
 import json
 from db_login import DB_CREDENTIALS
+from db_portal_login import PORTAL_CREDENTIALS
+import psycopg2
 
 logging.basicConfig(filename='db.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger('database')
@@ -19,18 +21,44 @@ class DB:
         self.version_hashes = {}
         self.repo_index = {}
         self.mount_points = []
+        self.db_portal_info = {}
         self.pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **DB_CREDENTIALS)
 
 @route('/up')
 def is_up():
     return {"status":"up"}
+@route('/get_update_info')
+def get_update_info():
+
+
+@route('/get_portal_info')
+def get_portal_info():
+    try:
+        connection = psycopg2.connect(
+            host="login.wipeos.com",
+            database="wipeos_portal",
+            user="wipeos",
+            password="WiPe332"
+        )
+        cursor = connection.cursor()
+        cursor.execute("SELECT row_to_json(t) FROM (SELECT name,system_id,(select value from wipers_settings where setting='siteid' and system_id=ww.system_id),to_char(last_sync, 'YYYY/MM/DD HH24:MI:SS') as last_sync,version from wipers_wipebox ww join account_account aa on aa.id=ww.account_id where last_sync>=current_date-90) t")
+        result = cursor.fetchall()
+        all_boxes_last_90_days = []
+        for r in result:
+            all_boxes_last_90_days.append(r[0])
+        return {"status": "success", "data": all_boxes_last_90_days}
+    except psycopg2.Error as error:
+        logger.error("Error while connecting to PostgreSQL", error) #error checking for actual connection?
+        return {"status": "failure", "message": str(error)}
+    finally: #end closing connection
+        if connection:
+            cursor.close()
+            connection.close()
 
 @route('/insert_wipebox', method='POST')
 def insert_wipebox():
     try:
         data = request.json
-        logger.info("Data received for insert_wipebox: {}".format(data))
-        logger.info(db.version_hashes)
         certificate = data.get('cert')
         version_id = data.get('hash')
         last_update = data.get('last_update')
@@ -38,6 +66,7 @@ def insert_wipebox():
         conn = get_db()
         cursor = conn.cursor()
         cursor.execute("INSERT INTO wipebox (cert, version_id, last_update) VALUES (%s, %s, %s)", (certificate, db.version_hashes[version_id], last_update))
+        last_row_id = cursor.lastrowid
         conn.commit()
         cursor.close()
         conn.close()
@@ -46,7 +75,7 @@ def insert_wipebox():
         response.status = 500
         return {"status": "error", "message": str(e)}
     logger.info("Wipebox inserted successfully with cert: %s", certificate)
-    return {"status": "success", "cert": certificate}
+    return {"status": "success", "cert": certificate, "id": last_row_id}
 
 def get_unique_wipeboxes():
     conn = get_db()
@@ -100,19 +129,52 @@ def get_unique_versions():
     conn.close()
     return versions
 
-@route('/insert_disk_usage/', method='POST')
+@route('/insert_disk_usage', method='POST')
 def insert_disk_usage():
     data = request.json
+    wipebox_id = data.get('wipebox_id')
     mount_point = data.get('mount_point')
-    size = data.get('size')
+    if mount_point not in db.mount_points:
+        response.status = 400
+        return {"status": "error", "message": "Mount point not found"}
+    total = data.get('total')
     used = data.get('used')
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO disk_usage (mountpoint, size, used) VALUES (%s, %s, %s)", (mount_point, size, used))
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return {"status": "success", "mountpoint": mountpoint}
+    available = data.get('available')
+    percent_used = data.get('percent_used')
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
+        logger.info("Inserting disk usage for wipebox_id: %s, mount_point: %s, total: %s, used: %s, available: %s, percent_used: %s", wipebox_id, mount_point, total, used, available, percent_used)
+        cursor.execute("INSERT INTO disk_usage (wipebox_id,mount_point_id, total, used, available, percent_used) VALUES (%s, %s, %s, %s, %s, %s)", (wipebox_id, db.mount_points[mount_point], total, used, available, percent_used))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return {"status": "success"}
+    except Exception as e:
+        logger.error("Error inserting disk usage: %s", str(e))
+        response.status = 500
+        return {"status": "error", "message": str(e)}
+
+@route('/insert_generic_config', method='POST')
+def insert_generic_config():
+    data = request.json
+    wipebox_id = data.get('wipebox_id')
+    name = data.get('name')
+    value = data.get('value')
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
+        logger.info("Inserting generic config for wipebox_id: %s, name: %s, value: %s", wipebox_id, name, json.dumps(value))
+        cursor.execute("INSERT INTO generic_config (wipebox_id, name, value) VALUES (%s, %s, %s)", (wipebox_id, name, json.dumps(value)))
+        conn.commit()
+        return {"status": "success"}
+    except Exception as e:
+        logger.error("Error inserting generic config: %s", str(e))
+        response.status = 500
+        return {"status": "error", "message": str(e)}
+    finally:
+        cursor.close()
+        conn.close()
 
 def get_mount_points():
     conn = get_db()
@@ -163,7 +225,7 @@ def get_version_hashes():
     cursor = conn.cursor()
     cursor.execute("select version_id,group_concat(hash order by repository_id separator '') from repo_version group by version_id")
     results = cursor.fetchall()
-    version_hashes = {}
+    version_hashes = {'-1': 1}
     for r in results:
         version_hashes[r[1]] = r[0]
     cursor.close()
@@ -186,4 +248,5 @@ if __name__ == '__main__':
     logger.info("Repository Index: %s", db.repo_index)
     db.mount_points = get_mount_points()
     logger.info("Mount Points: %s", db.mount_points)
-    run(host='0.0.0.0', port=8090, debug=True, quiet=True,reload=True)
+    db.db_portal_info = get_portal_info()
+    run(host='0.0.0.0', port=8090, debug=True, quiet=True,reloader=True)
